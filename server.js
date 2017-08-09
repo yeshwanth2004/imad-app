918b6df4dd0b1e7db22d45eda6da475f25f812e7
yeshwanth2004 <30826054+yeshwanth2004@users.noreply.github.com>
2017-08-09 11:14:19 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app,get('/article-one', function (req, res){
+   res.send('Artcle one is served ');    
+});
+app,get('/article-two', function (req, res){
+   res.send('Artcle two is served ');    
+});
+app,get('/article-three', function (req, res){
+   res.send('Artcle three is served ');    
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
