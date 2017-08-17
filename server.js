7cb92873adcba64397c5094ee21f9fe6c249a2a6
yeshwanth2004 <30826054+yeshwanth2004@users.noreply.github.com>
2017-08-17 11:26:11 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ app.get('/', function (req, res) {
 app,get('/article-one', function (req, res){
    res.send('Artcle one is served ');    
 });
-app,get('/article-two', function (req, res){
-   res.sendfile(path.join(__dirname, 'ui', 'article-two'));    
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
