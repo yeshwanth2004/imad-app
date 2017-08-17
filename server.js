ae6f532cfa381759bfde44cc7317275cba67afca
yeshwanth2004 <30826054+yeshwanth2004@users.noreply.github.com>
2017-08-17 11:26:47 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app,get('/article-one', function (req, res){
-   res.send('Artcle one is served ');    
-});
+
 
 
 app.get('/ui/style.css', function (req, res) {
